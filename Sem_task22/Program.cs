589a7fc8773b3a38d4563f831af80fa0574088c6
Aleksey Khorkov <60649756+LSubarin@users.noreply.github.com>
2023-03-05 18:43:49 +0300

    Sem_task22. Вводим число N выводим таблицу квадратов от 1 до N.

---
+++ Sem_task22/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача №22. Напишите программу которая принимает на вход число (N)
+// и выдает таблицк квадратов чисел от 1 до N.
+
+System.Console.WriteLine("введите число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine())); // Math.Abs - делает из отрицательного числа положительное
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write(Math.Pow(i, 2) + " ");
+}
