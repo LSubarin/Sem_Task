2ff81b06d597faad988c06369bb553d0712f5ff1
Aleksey Khorkov <60649756+LSubarin@users.noreply.github.com>
2023-03-05 18:25:53 +0300

    Sem_task1. № не было. Выводит случайное число и показывает наибольшую цифру. Random.

---
+++ Sem_task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+Random rand = new Random();
+int randNumber = rand.Next(10, 100);    // Генерируем случайное число из диапазона [10,100)
+System.Console.WriteLine(randNumber);   // Выводим в консоль полученное число   
+
+int leftNumber = randNumber / 10;       // Получаем левую цифру числа (Целое от деления на 10)
+int rightNumber = randNumber % 10;      // получаем правую цифру числа (Остаток от деления на 10)
+if (leftNumber > rightNumber)            // Если левое больше правого
+{
+    System.Console.WriteLine($"{leftNumber} > {rightNumber}"); // Выводим - левое больше правого
+}
+else if (rightNumber > leftNumber)       // Иначе если правое больше левого
+{
+    System.Console.WriteLine($"{rightNumber} > {leftNumber}"); // выводим - правое больше левого
+}
+else                                   // Иначе 
+{
+    System.Console.WriteLine("Числа равны"); // выводим - Числа равны
+}
