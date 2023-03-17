25ffe3941ce82886201a8d9f8cf69cdcccdf4c3e
Aleksey Khorkov <60649756+LSubarin@users.noreply.github.com>
2023-03-17 11:53:44 +0300

    Задача №31. Заполняли массив случайными числами. Считали суммы пол и отр элементов

---
+++ Sem_task24/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача №24. Напишите прогшрамму которая на входе
+принимает число А, а на выходе показывает сумму чисел
+от 1 до А
+*/
+System.Console.WriteLine("Введите число А: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+for(int i = 1; i <= number; i++)
+{
+    sum+=i;
+}
+System.Console.WriteLine($"Сумма чисел от 1 до {number} = {sum}");
